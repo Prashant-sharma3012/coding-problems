0356d698fe8eecd7c2a43762b0a03a4b9c239a5c
prashant sharma <prashant.sharma@enquero.com>
2020-03-03 16:23:44 +0530

    first problem to therepo, run length encoding

---
+++ easy/run-length-encoding.go
@@ -0,0 +1,39 @@
+// Run-length encoding is a fast and simple method of encoding strings. The basic idea is to represent repeated successive characters as a single count and character. For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+
+// Implement run-length encoding and decoding. You can assume the string to be encoded have no digits and consists solely of alphabetic characters. You can assume the string to be decoded is valid.
+
+package runlengthencoding
+
+import "strconv"
+
+func RunLengthEncode(input string) string {
+	encoded := ""
+	count := 1
+
+	for i := 0; i < len(input)-1; i++ {
+		if input[i] == input[i+1] {
+			count++
+		} else {
+			encoded = encoded + strconv.Itoa(count) + string(input[i])
+			count = 1
+		}
+	}
+
+	// for the last character
+	encoded = encoded + strconv.Itoa(count) + string(input[len(input)-1])
+
+	return encoded
+}
+
+func RunLengthDecode(input string) string {
+	decoded := ""
+
+	for i := 0; i < len(input); i = i + 2 {
+		num, _ := strconv.Atoi(string(input[i]))
+		for j := 0; j < num; j++ {
+			decoded = decoded + string(input[i+1])
+		}
+	}
+
+	return decoded
+}
